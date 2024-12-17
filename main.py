1df22f1bfb5d586af591454f7ceffea7cbdad388
kavianmaler <o.piltz@hotmail.com>
2024-12-17 17:12:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import infer_pipeline, freq_spec_pipeline
 
 if __name__ == "__main__":
     folder_creator.main()
-    api_tw_pipe.main()
+    #api_tw_pipe.main()
     infer_pipeline.main()
     freq_spec_pipeline.main()
     db_azure_con.main()
